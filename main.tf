26bb23b274e5bbab77ac8817dd981d9e642759c0
lanaymc <lanay@getupcloud.com>
2021-11-12 09:52:47 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+locals {
+  name_prefix = substr("${var.cluster_name}-external-dns", 0, 32)
+}
+
+data "aws_iam_policy_document" "aws_external-dns" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "route53:ChangeResourceRecordSets"
+    ]
+
+    resources = [
+      "arn:aws:route53:::hostedzone/${var.hosted_zone_id}"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListResourceRecordSets"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "aws_external-dns" {
+  name        = local.name_prefix
+  description = "External DNS policy for EKS cluster ${var.cluster_name}"
+  policy      = data.aws_iam_policy_document.aws_external-dns.json
+}
+
+module "irsa_aws_external-dns" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "~> 4.2"
+
+  create_role                   = true
+  role_name                     = local.name_prefix
+  provider_url                  = var.cluster_oidc_issuer_url
+  role_policy_arns              = [aws_iam_policy.aws_external-dns.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"]
+  tags                          = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+}
+variable "customer_name" {
+  description = "customer name"
+  type        = string
+}
+
+variable "hosted_zone_id" {
+  description = "AWS Route53 Hosted Zone ID to external dns automatically handle"
+  type        = string
+
+  validation {
+    condition     = length(var.hosted_zone_id) > 0
+    error_message = "The hosted_zone_id value must not be empty."
+  }
+}
+
+variable "cluster_oidc_issuer_url" {
+  description = "URL of the OIDC Provider from the EKS cluster"
+  type        = string
+}
+
+variable "service_account_namespace" {
+  description = "Namespace of ServiceAccount for external-dns"
+  default     = "external-dns"
+}
+
+variable "service_account_name" {
+  description = "ServiceAccount name for external-dns"
+  default     = "external-dns"
+}
+variable "tags" {
+  description = "AWS tags to apply to resources"
+  type        = any
+  default     = {}
+}
