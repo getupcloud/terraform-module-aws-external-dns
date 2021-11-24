bb60280943b263f5f9bc0a649dda759b264bfc74
lanaymc <lanay@getupcloud.com>
2021-11-24 12:20:06 -0300

    fix: role name prefix

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "aws_external-dns" {
 }
 
 resource "aws_iam_policy" "aws_external-dns" {
-  name        = "local.name_prefix-${var.prefix}"
+  name        = "${local.name_prefix}-${var.prefix}"
   description = "External DNS policy for EKS cluster ${var.cluster_name}"
   policy      = data.aws_iam_policy_document.aws_external-dns.json
 }
@@ -38,7 +38,7 @@ module "irsa_aws_external-dns" {
   version = "~> 4.2"
   
   create_role                   = true
-  role_name                     = "local.name_prefix-${var.prefix}"
+  role_name                     = "${local.name_prefix}-${var.prefix}"
   provider_url                  = var.cluster_oidc_issuer_url
   role_policy_arns              = [aws_iam_policy.aws_external-dns.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"]
