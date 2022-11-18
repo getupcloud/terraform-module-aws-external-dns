c8ed4d867582a9f7a0376bb9d74820cadf613068
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-18 11:06:38 -0300

    Adding outputs

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   name_prefix = substr("${var.cluster_name}-external-dns", 0, 32)
 }
 
-data "aws_iam_policy_document" "aws_external-dns" {
+data "aws_iam_policy_document" "aws_external_dns" {
   statement {
     effect = "Allow"
 
@@ -27,20 +27,20 @@ data "aws_iam_policy_document" "aws_external-dns" {
   }
 }
 
-resource "aws_iam_policy" "aws_external-dns" {
+resource "aws_iam_policy" "aws_external_dns" {
   name        = "${local.name_prefix}-${var.prefix}"
   description = "External DNS policy for EKS cluster ${var.cluster_name}"
-  policy      = data.aws_iam_policy_document.aws_external-dns.json
+  policy      = data.aws_iam_policy_document.aws_external_dns.json
 }
 
-module "irsa_aws_external-dns" {
+module "irsa_aws_external_dns" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 4.2"
-  
+
   create_role                   = true
   role_name                     = "${local.name_prefix}-${var.prefix}"
   provider_url                  = var.cluster_oidc_issuer_url
-  role_policy_arns              = [aws_iam_policy.aws_external-dns.arn]
+  role_policy_arns              = [aws_iam_policy.aws_external_dns.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"]
   tags                          = var.tags
 }
